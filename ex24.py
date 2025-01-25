493af16fcb21a068a368da98029c3e1a643526aa
Mike Stewart <mikestewart.dev@gmail.com>
2025-01-24 23:02:06 -0700

    More updates to exersize 24

--- ex24.py
+++ ex24.py
@@ -26,3 +26,6 @@ def secret_formula(started):
 
 start_point = 10000
 beans, jars, crates = secret_formula(start_point)
+
+# remember that this is another way to format a string
+print("With a starting point of: {}". format(start_point))
