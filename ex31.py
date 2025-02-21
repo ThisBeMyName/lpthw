c1ea09e62673eb0ee2b7bb6958933919881fb0ef
Mike Stewart <mikestewart.dev@gmail.com>
2025-02-21 13:35:01 -0700

    Updating 'ex31.py' with study drills

--- ex31.py
+++ ex31.py
@@ -14,7 +14,7 @@ if door == "1":
     print("6. Try to reason with the bear and pet it.")
     print("7. Try to reason with the bear and take the cake.")
 
-bear = input("> ")
+    bear = input("> ")
 
     if bear == "1":
         print("The bear eats your face off. Good job!")
@@ -24,3 +24,55 @@ bear = input("> ")
         print("You run away and the bear chases you down and eats you.")
     elif bear == "4":
         print("The bear mauls you.")
+    elif bear == "5":
+        print("The bear speaks philosophy and you die of boredom.")
+    elif bear == "6":
+        print("The bear speaks on the finer points of quantum physics then mauls you for trying to pet it.")
+    elif bear =="7":
+        print("While the bear is busy speaking on the teachings of Plato, you take the cake and run away.")
+    else:
+        print(f"The bear is confused watching you do {bear} and decides to maul you.")
+
+elif door == "2":
+    print("You enter a large room with some goblins. They stare at you in suprise, then grab their spears.")
+    print("What do you do?")
+    print("1. Throw a rock.")
+    print("2. Run away.")
+    print("3. Shout at them.")
+    print("4. Run at them and grab the closest's spear.")
+
+    goblin = input("> ")
+
+    if goblin == "1" or goblin == "2":
+        print("The goblins laugh at you and throw the rock back.")
+        print("You get hit in the head and die.")
+    elif goblin == "3":
+        print("The goblins are startled, then stick you like a pig.")
+        print("You die.")
+    elif goblin == "4":
+        print("You grab the spear and the goblins are impressed.")
+        print("They give you a choice of weapons to duel with.")
+        print("1. Sword")
+        print("2. Axe")
+        print("3. Bow")
+        
+        weapon = input("> ")
+
+        if weapon == "1":
+            print("You grab the sword and the goblins give you a shield.")
+            print("You fight valiantly but are outnumbered.")
+            print("You die.")
+        elif weapon == "2":
+            print("You grab the axe and the goblins give you a shield.")
+            print("You strike many down but are outnumbered.")
+            print("You die.")
+        elif weapon == "3":
+            print("You grab the bow and the goblins give you a quiver of arrows.")
+            print("You shoot many goblins but are outnumbered.")
+            print("You die.")
+        else:
+            print("The goblins are confused. They Kill you.")
+    else:
+        print("The goblins are confused. They Kill you.")
+else:
+    print("You stumble around and fall on a knife and die.  Good job!")
